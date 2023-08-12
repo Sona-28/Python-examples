c37c38aaab95a3c1626def16729e6ba53a025e06
Your Name <you@example.com>
2023-08-12 11:27:45 +0530

    Added some files

---
+++ python/sample.py
@@ -0,0 +1,10 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+set1={1,2,2,2,3,4,4,}
+print(set1)
+{1, 2, 3, 4}
+set1.add(100)
+print(set1)
+{1, 2, 3, 100, 4}
+s1={
+    
